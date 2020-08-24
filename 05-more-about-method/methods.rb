e015b80ec93d6ff51b53bbadabe120740dd75e0b
Cloneable <cloneable@163.com>
2020-08-24 21:47:07 +0800

    finish examples of chapter 05 section Defining a Method

---
+++ 05-more-about-method/methods.rb
@@ -0,0 +1,44 @@
+def coolDude(arg1 = "Miles", arg2 = "Coltrane", arg3 = "Roach")
+  "#{arg1}, #{arg2}, #{arg3}."
+end
+
+puts coolDude
+puts coolDude("Bart")
+puts coolDude("Bart", "Elwood")
+puts coolDude("Bart", "Elwood", "Linus")
+puts
+
+def varargs(arg1, *rest)
+  "Got #{arg1} and #{rest.join(', ')}"
+end
+
+puts varargs("one")
+puts varargs("one", "two")
+puts varargs "one", "two", "three"
+puts
+
+def takeBlock(p1)
+  if block_given?
+    yield(p1)
+  else
+    p1
+  end
+end
+
+puts takeBlock("no block")
+puts takeBlock("no block") {|s| s.sub(/no /, '')}
+puts
+
+class TaxCalculator
+  def initialize(name, &block)
+    @name, @block = name, block
+  end
+
+  def getTax(amount)
+    "#@name on #{amount} = #{@block.call(amount)}"
+  end
+end
+
+tc = TaxCalculator.new("Sales tax") {|amt| amt * 0.075}
+puts tc.getTax(100)
+puts tc.getTax(250)
